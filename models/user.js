face134887bce57e64291c928d6978387d396c0b
kolaster123 <carbajalruben1563@gmail.com>
2024-07-11 21:46:26 -0700

    Car part Schema added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 
+const CarpartSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['got it!', 'dont got it!','saving up'],
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +27,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [CarpartSchema],
 });
 
 const User = mongoose.model('User', userSchema);
